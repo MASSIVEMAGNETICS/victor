65f6ffd0fe3c934c42798d4dd85b71eec64f2d98
IAMBANDOBANDZ <massivemagnetics@gmail.com>
2026-08-25 02:00:51 -0400

    Add cognitive ecology package exports

---
+++ experiments/victor_cognitive_ecology/cognitive_ecology/__init__.py
@@ -0,0 +1,21 @@
+from .ecology import (
+    AetherlingPromotionProposal,
+    Budget,
+    CognitiveEcology,
+    CognitiveResult,
+    CognitiveTask,
+    GEVObservation,
+    ProcessStats,
+    SpawnRequest,
+)
+
+__all__ = [
+    "AetherlingPromotionProposal",
+    "Budget",
+    "CognitiveEcology",
+    "CognitiveResult",
+    "CognitiveTask",
+    "GEVObservation",
+    "ProcessStats",
+    "SpawnRequest",
+]
